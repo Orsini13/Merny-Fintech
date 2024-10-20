c35b14e42691764f58e9f680ceba9179f435c4c8
Udeh Udochukwu <udochukwugodwin13@gmail.com>
2024-10-20 02:51:21 +0100

    implement shadcn mobnav

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,6 @@
+import MobileNav from "@/components/MobileNav";
 import Sidebar from "@/components/Sidebar";
+import Image from "next/image";
 
 export default function RootLayout({
   children,
@@ -6,12 +8,26 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
 
-  const loggedIn = {firstName: 'Adrian', lastName:'JSM'};
+  const loggedIn = { firstName: 'Adrian', lastName: 'JSM' };
 
   return (
     <main className="flex h-screen w-full font-inter">
       <Sidebar user={loggedIn} />
+      <div className="flex size-full flex-col">
+        <div className="root-layout">
+          <Image src="/icons/logoo.jpeg" width={30} height={30} alt={"menu icon"} />
+          <div>
+            <MobileNav user={loggedIn} />
+          </div>
+        </div>
       {children}
+      </div>
     </main>
   )
 }
+// async function sleep(millis) {
+//   setTimeout(() => {
+//     console.log('big dog')
+//   }, millis)
+// }
+// sleep(100);
