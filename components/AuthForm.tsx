9e75d819050c6dab1db876ac0287c6458bc65a90
Udeh Udochukwu <udochukwugodwin13@gmail.com>
2024-10-22 06:18:19 +0100

    set appwrite funcs

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -24,7 +24,6 @@ import CustomInput from './CustomInput'
 import { authFormSchema } from '@/lib/utils'
 import { Loader2 } from 'lucide-react'
 import { useRouter } from 'next/navigation'
-import { signIn, signUp } from '@/lib/actions/user.actions'
 
 
 
@@ -47,14 +46,14 @@ const AuthForm = ({ type }: { type: string }) => {
     try {
       //Sign up with Appwrite & create a plaid token
       if (type === 'sign-up') {
-        const newUser = await signUp(data);
-        setUser(newUser);
+        // const newUser = await signUp(data);
+        // setUser(newUser);
       }
       if (type === 'sign-in') {
-        const response = await signIn({
-          email: data.email,
-          password: data.password})
-        if (response) router.push('/')
+        // const response = await signIn({
+        //   email: data.email,
+        //   password: data.password})
+        // if (response) router.push('/')
       }
     }
     catch (error) {
